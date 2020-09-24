659b0c797714adf27ab6016e350a681775415d5f
DESKTOP-MU3H65L\Nischita <nisch.s@gmail.com>
2020-09-24 18:21:19 +0200

    Anchoring objects using HoloLens 2 using local anchors

---
+++ SpatialMappingH2/Assets/DebugText.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DebugText : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshPro textMeshPro = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMeshPro.text = "test test";
+        Application.logMessageReceived += HandleLog;
+
+    }
+
+
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+
+        textMeshPro.text += logString + "\n";
+    
+    }
+
+}
